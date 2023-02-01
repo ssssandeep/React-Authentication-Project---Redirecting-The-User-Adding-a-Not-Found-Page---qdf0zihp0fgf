76858a9f7ea6a9881c1ceb519a18a551e4947872
sandeepitgcet <sandeepitgcet@gmail.com>
2023-02-01 13:19:22 +0530

    updaeteed

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom';
 
+const PrivateRoute = ({children}) => {
+    const user= window.localStorage.getItem('username');
+    const pass = window.localStorage.getItem('password');
+    if(user === 'abc' && pass === '123'){
+        return children;
+    }
+    console.log("id and pass not match");
+  return <Redirect to="/login" />;
+}
+
+export default PrivateRoute
